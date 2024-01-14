fb9d61a6610b3dc111ebedd401bff94e3d9ecf61
medicationforall <inklink28@gmail.com>
2024-01-14 15:34:36 -0500

    Added portal build_open method.

--- example/portal.py
+++ example/portal.py
@@ -2,14 +2,20 @@ import cadquery as cq
 from cqportal import Portal
 
 bp_portal = Portal()
-bp_portal.bp_frame.length = 140
+bp_portal.bp_frame.length = 150
 bp_portal.bp_frame.width = 30
-bp_portal.bp_frame.height = 200
+bp_portal.bp_frame.height = 150
 
 bp_portal.render_ramps = True
 bp_portal.ramp_push = 0
 bp_portal.bp_ramp.width = 10
 bp_portal.make()
 
-result = bp_portal.build_closed()
-cq.exporters.export(result, 'stl/portal.stl')
+result_closed = bp_portal.build_closed()
+#show_object(result_closed.translate((-170,0,0)))
+
+result_open = bp_portal.build_open()
+#show_object(result_open)
+
+cq.exporters.export(result_closed, 'stl/portal_closed.stl')
+cq.exporters.export(result_open, 'stl/portal_open.stl')
--- src/cqportal/Portal.py
+++ src/cqportal/Portal.py
@@ -28,6 +28,7 @@ class Portal(Base):
         self.bp_frame = Frame()
         self.bp_ramp = Ramp()
         self.ramp_push = 0
+        self.open_angle = 94
         
     def make(self, parent=None):
         super().make(parent)
@@ -59,6 +60,38 @@ class Portal(Base):
                     ramp
                     .rotate((0,0,1),(0,0,0), 180)
                     .translate((0,(self.bp_frame.width/2+self.ramp_push),self.bp_ramp.height/2+self.bp_base.height))
+                )
+            )
+        
+        return scene
+    
+    def build_open(self):
+        super().build()
+        portal_base  = self.bp_base.build()
+        frame = self.bp_frame.build()
+        
+        scene = (
+            cq.Workplane("XY")
+            .union(portal_base.translate((0,0,self.bp_base.height/2)))
+            .union(frame.translate((0,0,self.bp_frame.height/2 + self.bp_base.height)))
+
+        )
+        
+        if self.render_ramps:
+            ramp = self.bp_ramp.build()
+            scene = (
+                scene
+                .add(
+                    ramp
+                    .rotate((1,0,0),(0,0,0),-self.open_angle)
+                    .translate((0,-1*(self.bp_ramp.height/2 + self.bp_frame.width/2),self.bp_ramp.width/2+self.bp_base.height/2))
+                    #.translate((0,-1*(self.bp_frame.width/2+self.ramp_push),self.bp_ramp.height/2+self.bp_base.height))
+                )
+                .add(
+                    ramp
+                    .rotate((0,0,1),(0,0,0), 180)
+                    .rotate((1,0,0),(0,0,0),self.open_angle)
+                    .translate((0,(self.bp_ramp.height/2 + self.bp_frame.width/2),self.bp_ramp.width/2+self.bp_base.height/2))
                 )
             )
         
