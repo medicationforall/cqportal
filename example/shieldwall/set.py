ac485c1b16504a2595dd55ac86dbf24e153b551f
medicationforall <inklink28@gmail.com>
2024-08-01 05:24:24 -0400

    Updated shieldwall set example parameters.

--- example/shieldwall/set.py
+++ example/shieldwall/set.py
@@ -2,6 +2,19 @@ import cadquery as cq
 from cqportal.shieldwall import Set
 
 set_bp = Set()
+
+set_bp.straight_count = 4
+set_bp.padding = 5
+set_bp.height = 25
+
+set_bp.base_height = 5.6
+set_bp.magnet_padding_x = 2
+
+set_bp.corner_count = 2
+
+set_bp.end_cap_count = 4
+set_bp.end_cap_length = 15
+
 set_bp.make()
 set_ex = set_bp.build()
 
